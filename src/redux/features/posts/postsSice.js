7f90203216f946a693246e4240ae3d7a5e32878e
Guilherme <guilhermemagno09@gmail.com>
2023-02-22 23:42:24 -0300

    adding actions

--- src/redux/features/posts/postsSice.js
+++ src/redux/features/posts/postsSice.js
@@ -7,12 +7,26 @@ const initialState = [
     title: "Learing Redux Toolkit",
     content: "Redux Toolkit the best Global state",
     date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsup: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffe: 0,
+    },
   },
   {
     id: 2,
     title: "Slices",
     content: "Slice always remind me of pizza",
     date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsup: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffe: 0,
+    },
   },
 ];
 
@@ -32,9 +46,23 @@ const postsSlice = createSlice({
             content,
             date: new Date().toISOString(),
             authorId,
+            reactions: {
+              thumbsup: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffe: 0,
+            },
           },
         };
       },
+    },
+    reactionAdded: (state, action) => {
+      const { postId, reaction } = action.payload;
+      const existentPost = state.find((post) => post.id === postId);
+      if(existentPost) {
+        existentPost.reactions[reaction]++
+      }
     },
   },
 });
